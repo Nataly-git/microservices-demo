1112f429c88cb853d6d508d45423b330e6f489b1
NatalyTereshkova <NatalyTereshkova@coherentsolutions.com>
2023-05-22 13:57:56 +0200

    added twitter streaming

---
+++ twitter-to-kafka-serivice/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,32 @@
+package com.microservices.demo.twitter.to.kafka.service;
+
+import com.microservices.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import com.microservices.demo.twitter.to.kafka.service.runner.StreamRunner;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+@Slf4j
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+    private final TwitterToKafkaServiceConfigData configData;
+    private final StreamRunner streamRunner;
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("App starts...");
+        log.info(Arrays.toString(configData.getTwitterKeywords().toArray()));
+        log.info(configData.getWelcomeMessage());
+        streamRunner.start();
+    }
+}
